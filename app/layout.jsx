3012213deb6223a3914770b76bcd9c774b3681bd
Hector Florez <hectorflorez25@gmail.com>
2026-05-03 21:59:50 -0500

    Responsive: sidebar drawer, topbar y tablas adaptadas a móvil

--- app/layout.jsx
+++ app/layout.jsx
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
